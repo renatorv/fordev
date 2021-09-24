354074756dc2622e6889b097b6fd33388d8b5aa1
Renato Alves <renato@volus.com>
2021-09-24 19:03:32 -0300

    ensure RemoteAuthentication calls HttpClient with correct URL.

---
+++ test/data/usecases/remote_authentication_test.dart
@@ -0,0 +1,42 @@
+import 'package:faker/faker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class RemoteAuthentication {
+  final HttpClient httpClient;
+  final String url;
+
+  RemoteAuthentication({required this.httpClient, required this.url});
+
+  Future<void> auth() async {
+    await httpClient.request(url: url);
+  }
+}
+
+abstract class HttpClient {
+  Future<void> request({
+    required String url,
+  });
+}
+
+class HttpClientSpy extends Mock implements HttpClient{}
+
+void main() {
+  test(
+    'Should call HttpClient with corret URL',
+    () async {
+      final httpClient = HttpClientSpy();
+      final url = faker.internet.httpUrl();
+
+      /// Design Pattern 3A
+      /// Arrange: onde o teste é organizado
+      final sut = RemoteAuthentication(httpClient: httpClient, url: url);
+
+      /// Act: Ação
+      sut.auth();
+
+      /// Accert
+      verify(httpClient.request(url: url));
+    },
+  );
+}
