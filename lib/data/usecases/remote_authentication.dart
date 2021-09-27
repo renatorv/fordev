e8ee1dbbb4e9390c8d58743a6752f8969337e24e
Renato Alves <renato@volus.com>
2021-09-27 15:12:51 -0300

    ensure RemoteAuthentication throws UnexpectedError on 400

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -1,3 +1,5 @@
+import '../../domain/helpers/helpers.dart';
+
 import '../../domain/usecases/usecases.dart';
 
 import '../http/http.dart';
@@ -10,7 +12,12 @@ class RemoteAuthentication {
 
   Future<void> auth(AuthenticationsParams params) async {
     final body = RemoteAuthenticationsParams.fromDomain(params).toJson();
-    await httpClient.request(url: url, method: 'post', body: body);
+
+    try {
+      await httpClient.request(url: url, method: 'post', body: body);
+    } on HttpError {
+      throw DomainError.unexpected;
+    }
   }
 }
 
@@ -23,13 +30,14 @@ class RemoteAuthenticationsParams {
     required this.password,
   });
 
-  factory RemoteAuthenticationsParams.fromDomain(AuthenticationsParams params) =>
-    RemoteAuthenticationsParams(email: params.email, password: params.email);
+  factory RemoteAuthenticationsParams.fromDomain(
+          AuthenticationsParams params) =>
+      RemoteAuthenticationsParams(email: params.email, password: params.email);
 
   // Criação, na aula: 4 - Testando o body do request
   /// o clean arquitecture não recomenda o envio de nomes de campos
   /// fixado como no map abaixo ( email e password ), pois nem caso de
-  /// alguma mudança de API, ex. password para pwd geraria problemas 
+  /// alguma mudança de API, ex. password para pwd geraria problemas
   /// de compatibilidade.
   Map toJson() => {'email': email, 'password': password};
 }
--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -2,27 +2,9 @@ import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:fordev/data/http/http_client.dart';
 import 'package:fordev/data/usecases/usecases.dart';
+import 'package:fordev/domain/helpers/helpers.dart';
 import 'package:fordev/domain/usecases/authentication.dart';
 import 'package:mockito/mockito.dart';
-
-// class RemoteAuthentication {
-//   final HttpClient httpClient;
-//   final String url;
-
-//   RemoteAuthentication({required this.httpClient, required this.url});
-
-//   Future<void> auth(AuthenticationsParams params) async {
-//     await httpClient.request(url: url, method: 'post', body: params.toJson());
-//   }
-// }
-
-// abstract class HttpClient {
-//   Future<void> request({
-//     required String url,
-//     required String method,
-//     Map body,
-//   });
-// }
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
@@ -52,6 +34,29 @@ void main() {
             method: 'post',
             body: {'email': params.email, 'password': params.secret}),
       );
+    },
+  );
+
+  test(
+    'Should throw UnexpectedError if HttpClient return 400',
+    () async {
+      
+      final httpClient = HttpClientSpy();
+      final url = faker.internet.httpUrl();
+
+      when(httpClient.request(url: anyNamed('url'), method: anyNamed('method'), body: anyNamed('body')))
+        .thenThrow(HttpError.badRequest);
+
+      final params = AuthenticationsParams(
+        email: faker.internet.email(),
+        secret: faker.internet.password(),
+      );
+
+      final sut = RemoteAuthentication(httpClient: httpClient, url: url);
+
+      final future = sut.auth(params);
+
+      expect(future, throwsA(DomainError.unexpected));
     },
   );
 }
