e74448db1bb6fe573dcd1a226c874f639d254e67
Renato Alves <renato@volus.com>
2021-09-27 11:37:09 -0300

    move API details from domain class to data class

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -9,6 +9,27 @@ class RemoteAuthentication {
   RemoteAuthentication({required this.httpClient, required this.url});
 
   Future<void> auth(AuthenticationsParams params) async {
-    await httpClient.request(url: url, method: 'post', body: params.toJson());
+    final body = RemoteAuthenticationsParams.fromDomain(params).toJson();
+    await httpClient.request(url: url, method: 'post', body: body);
   }
+}
+
+class RemoteAuthenticationsParams {
+  final String email;
+  final String password;
+
+  RemoteAuthenticationsParams({
+    required this.email,
+    required this.password,
+  });
+
+  factory RemoteAuthenticationsParams.fromDomain(AuthenticationsParams params) =>
+    RemoteAuthenticationsParams(email: params.email, password: params.email);
+
+  // Criação, na aula: 4 - Testando o body do request
+  /// o clean arquitecture não recomenda o envio de nomes de campos
+  /// fixado como no map abaixo ( email e password ), pois nem caso de
+  /// alguma mudança de API, ex. password para pwd geraria problemas 
+  /// de compatibilidade.
+  Map toJson() => {'email': email, 'password': password};
 }
