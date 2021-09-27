7aa3df65d8d8173eef29854f7808fa345451328a
Renato Alves <renato@volus.com>
2021-09-27 11:05:30 -0300

    move production code to their own target

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -1,27 +1,28 @@
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:fordev/data/http/http_client.dart';
+import 'package:fordev/data/usecases/usecases.dart';
 import 'package:fordev/domain/usecases/authentication.dart';
 import 'package:mockito/mockito.dart';
 
-class RemoteAuthentication {
-  final HttpClient httpClient;
-  final String url;
+// class RemoteAuthentication {
+//   final HttpClient httpClient;
+//   final String url;
 
-  RemoteAuthentication({required this.httpClient, required this.url});
+//   RemoteAuthentication({required this.httpClient, required this.url});
 
-  Future<void> auth(AuthenticationsParams params) async {
-    final body = {'email': params.email, 'password': params.secret};
-    await httpClient.request(url: url, method: 'post', body: body);
-  }
-}
+//   Future<void> auth(AuthenticationsParams params) async {
+//     await httpClient.request(url: url, method: 'post', body: params.toJson());
+//   }
+// }
 
-abstract class HttpClient {
-  Future<void> request({
-    required String url,
-    required String method,
-    Map body,
-  });
-}
+// abstract class HttpClient {
+//   Future<void> request({
+//     required String url,
+//     required String method,
+//     Map body,
+//   });
+// }
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
