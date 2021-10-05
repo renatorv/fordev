990da4b9aadaf8d930f0a638056190dc62211b7a
Renato Alves <renato@volus.com>
2021-10-05 08:27:00 -0300

    final da aula - 2. Testando casos de sucesso

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,15 +1,16 @@
 import 'dart:convert';
 import 'package:faker/faker.dart';
+import 'package:fordev/data/http/http_client.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
-class HttpAdapter {
+class HttpAdapter implements HttpClient {
   final Client client;
 
   HttpAdapter(this.client);
 
-  Future<void> request({
+  Future<Map> request({
     required String url,
     required String method,
     required Map body,
@@ -18,7 +19,14 @@ class HttpAdapter {
       'content-type': 'application/json',
       'accept': 'application/json'
     };
-    await client.post(Uri.parse(url), headers: headers, body: jsonEncode(body));
+    final response = await client.post(Uri.parse(url),
+        headers: headers, body: jsonEncode(body));
+
+    if(response.statusCode == 200){
+      return response.body.isEmpty ? null : jsonDecode(response.body);
+    }else{
+      return null;
+    }
   }
 }
 
@@ -34,14 +42,25 @@ void main() {
       await sut
           .request(url: url, method: 'post', body: {'any_key': 'any_value'});
 
-      verify(client.post(
-        Uri.parse(url),
-        headers: {
-          'content-type': 'application/json',
-          'accept': 'application/json'
-        },
-        body: '{"any_key":"any_value"}'
-      ));
+      verify(client.post(Uri.parse(url),
+          headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json'
+          },
+          body: '{"any_key":"any_value"}'));
+    });
+
+    test('Shoul return data if post returns 200', () async {
+      final client = ClientSpy();
+      final sut = HttpAdapter(client);
+      final url = faker.internet.httpUrl();
+
+      when(client.post(url, headers: anyNamed('headers')))
+          .thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
+
+      final response = await sut.request(url: url, method: 'post', body: {'any_key': 'any_value'});
+
+      expect('any_key', 'any_value');
     });
   });
 }
